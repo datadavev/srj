b5a3d5e2d91c468c2944bc3804d0055cc40bdf20
datadavev <605409+datadavev@users.noreply.github.com>
2021-09-28 22:50:21 -0400

    db?

--- api/index.py
+++ api/index.py
@@ -1,10 +1,18 @@
 from cowpy import cow
 from flask import Flask, Response
+import sqlite3
+
 app = Flask(__name__)
+
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
+    con = sqlite3.connect('example.db')
+    cur = con.cursor()
+    msg = []
+    for row in cur.execute('SELECT * FROM stocks ORDER BY price'):
+        msg.append(str(row))
     message = cow.Cowacter().milk('Hello from Python from a simple Serverless Function!')
-    return Response(f"<pre>{message}</pre>", mimetype="text/html")
+    return Response(f"<pre>{message}\n{'\n'.join(msg)}</pre>", mimetype="text/html")
 
