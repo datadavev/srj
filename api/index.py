19632fada2a4c5f632bc0216aa38e8334b2950e0
datadavev <605409+datadavev@users.noreply.github.com>
2021-10-04 11:23:43 -0400

    update

--- api/index.py
+++ api/index.py
@@ -1,18 +1,52 @@
-from cowpy import cow
+import subprocess
+import sys
+import platform
 from flask import Flask, Response
+import pkg_resources
+import sysconfig
+import json
 import sqlite3
 
 app = Flask(__name__)
 
+def system_call(command):
+    """
+    params:
+        command: list of strings, ex. `["ls", "-l"]`
+    returns: output, success
+    """
+    try:
+        output = subprocess.check_output(command, stderr=subprocess.STDOUT).decode()
+        success = True
+    except subprocess.CalledProcessError as e:
+        output = e.output.decode()
+        success = False
+    return output, success
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    con = sqlite3.connect('example.db')
-    cur = con.cursor()
+    j = "\n"
     msg = []
-    for row in cur.execute('SELECT * FROM stocks ORDER BY price'):
-        msg.append(str(row))
-    message = cow.Cowacter().milk('Hello from Python from a simple Serverless Function!')
-    return Response(f"<pre>{message}\n{'|'.join(msg)}</pre>", mimetype="text/html")
+    msg.append(platform.platform())
+    msg.append(f"python {sys.version_info.major}.{sys.version_info.minor}")
+    msg.append(f"{j.join(sys.path)}")
+    msg.append("---")
+    _paths = sysconfig.get_paths()
+    msg.append(f"{json.dumps(_paths, indent=2)}")
+    msg.append("---")
+    output, success = system_call(["ls", "-l", "/var/lang/lib/python3.6"])
+    msg.append(output)
+    msg.append("---")
+    output, success = system_call(["yum", "list"])
+    msg.append(output)
+    msg.append("---")
+    output, success = system_call(["ls", "-l"])
+    msg.append(output)
+    output,success = system_call(["sqlite3", "--version"])
+    msg.append(output)
+    output,success = system_call(["which", "python"])
+    msg.append(output)
 
+    msg += {d.project_name: d.version for d in pkg_resources.working_set}
+    return Response(f"<pre>{j.join(msg)}</pre>", mimetype="text/html")
