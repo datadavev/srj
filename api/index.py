a81d671fd5841c99f0e089ca9733b40c846f5dd6
datadavev <605409+datadavev@users.noreply.github.com>
2021-10-04 12:35:53 -0400

    remove flask

--- api/index.py
+++ api/index.py
@@ -1,13 +1,12 @@
 import subprocess
 import sys
 import platform
-from flask import Flask, Response
+from http.server import BaseHTTPRequestHandler
 import pkg_resources
 import sysconfig
 import json
 import sqlite3
 
-app = Flask(__name__)
 
 def system_call(command):
     """
@@ -23,30 +22,34 @@ def system_call(command):
         success = False
     return output, success
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    j = "\n"
-    msg = []
-    msg.append(platform.platform())
-    msg.append(f"python {sys.version_info.major}.{sys.version_info.minor}")
-    msg.append(f"{j.join(sys.path)}")
-    msg.append("---")
-    _paths = sysconfig.get_paths()
-    msg.append(f"{json.dumps(_paths, indent=2)}")
-    msg.append("---")
-    output, success = system_call(["ls", "-l", "/var/lang/lib/python3.6"])
-    msg.append(output)
-    msg.append("---")
-    output, success = system_call(["yum", "list"])
-    msg.append(output)
-    msg.append("---")
-    output, success = system_call(["ls", "-l"])
-    msg.append(output)
-    output,success = system_call(["sqlite3", "--version"])
-    msg.append(output)
-    output,success = system_call(["which", "python"])
-    msg.append(output)
 
-    msg += {d.project_name: d.version for d in pkg_resources.working_set}
-    return Response(f"<pre>{j.join(msg)}</pre>", mimetype="text/html")
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        j = "\n"
+        msg = []
+        msg.append(platform.platform())
+        msg.append(f"python {sys.version_info.major}.{sys.version_info.minor}")
+        msg.append(f"{j.join(sys.path)}")
+        msg.append("---")
+        _paths = sysconfig.get_paths()
+        msg.append(f"{json.dumps(_paths, indent=2)}")
+        msg.append("---")
+        output, success = system_call(["ls", "-l", "/var/lang/lib/python3.6"])
+        msg.append(output)
+        msg.append("---")
+        output, success = system_call(["yum", "list"])
+        msg.append(output)
+        msg.append("---")
+        output, success = system_call(["ls", "-l"])
+        msg.append(output)
+        output,success = system_call(["sqlite3", "--version"])
+        msg.append(output)
+        output,success = system_call(["which", "python"])
+        msg.append(output)
+
+        msg += {d.project_name: d.version for d in pkg_resources.working_set}
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(f"<pre>{j.join(msg)}</pre>".encode())
